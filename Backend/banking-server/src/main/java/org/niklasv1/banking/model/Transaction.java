1023c3eb1c228025f4bb5ac1fd49c1c277ad6384
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-15 16:53:49 +0100

    Transaction Entity

--- Backend/banking-server/src/main/java/org/niklasv1/banking/model/Transaction.java
+++ Backend/banking-server/src/main/java/org/niklasv1/banking/model/Transaction.java
@@ -1,4 +1,32 @@
 package org.niklasv1.banking.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transactions")
 public class Transaction {
+
+    @Id
+    @GeneratedValue
+    public UUID id;
+
+    @Column(nullable = false)
+    public String message;
+
+    @Column(nullable = false)
+    public Long amount;
+
+    @Column(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Account sender;
+
+    @Column(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Account receiver;
+
+    @Column(nullable = false)
+    public LocalDateTime timestamp;
 }
