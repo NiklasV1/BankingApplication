9d98e4601ff6add77e490d757c5443b197bb15ea
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-15 22:11:42 +0100

    create and get all accounts

--- Backend/banking-server/src/main/java/org/niklasv1/banking/account/AccountController.java
+++ Backend/banking-server/src/main/java/org/niklasv1/banking/account/AccountController.java
@@ -1,8 +1,13 @@
 package org.niklasv1.banking.account;
 
+import io.quarkus.hibernate.orm.panache.Panache;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import org.niklasv1.banking.customer.Customer;
+
+import java.util.List;
+import java.util.UUID;
 
 @ApplicationScoped
 @Transactional
@@ -10,4 +15,14 @@ public class AccountController {
 
     @Inject
     AccountRepository accountRepository;
+
+    public List<Account> getAllAccounts() {
+        return accountRepository.listAll();
+    }
+
+    public UUID createAccount(Customer customer, String name) {
+        Account account = new Account(customer, name);
+        accountRepository.persist(account);
+        return account.getId();
+    }
 }
--- Backend/banking-server/src/main/java/org/niklasv1/banking/account/AccountResource.java
+++ Backend/banking-server/src/main/java/org/niklasv1/banking/account/AccountResource.java
@@ -2,9 +2,15 @@ package org.niklasv1.banking.account;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import org.niklasv1.banking.AuthData;
 import org.niklasv1.banking.BankController;
+
+import java.util.List;
+import java.util.UUID;
 
 @Path("/api/account")
 @ApplicationScoped
@@ -16,5 +22,24 @@ public class AccountResource {
     @GET
     public String test() {
         return "account";
+    }
+
+    @GET
+    @Path("/all")
+    public List<Account> getAllAccounts() {
+        return bankController.getAllAccounts();
+    }
+
+    @POST
+    @Path("/create")
+    public UUID createAccount(AccountFormData accountFormData) {
+
+        AuthData authData = new AuthData(
+                accountFormData.id(),
+                accountFormData.username(),
+                accountFormData.plainPassword()
+        );
+
+        return bankController.createAccount(authData, accountFormData.name());
     }
 }
