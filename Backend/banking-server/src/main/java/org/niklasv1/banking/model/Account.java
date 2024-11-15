91f431490ef92fd0c66ab736c0a0ac1cef50e421
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-15 16:50:18 +0100

    Account Entity

--- Backend/banking-server/src/main/java/org/niklasv1/banking/model/Account.java
+++ Backend/banking-server/src/main/java/org/niklasv1/banking/model/Account.java
@@ -1,4 +1,26 @@
 package org.niklasv1.banking.model;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "accounts")
 public class Account {
+    @Id
+    @GeneratedValue
+    public UUID id;
+
+    @Column(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Customer owner;
+
+    @Column(nullable = false, length = 100)
+    public String name;
+
+    @Column(nullable = false)
+    public Long balance;
+
+    @Column(nullable = false)
+    public boolean frozen;
 }
