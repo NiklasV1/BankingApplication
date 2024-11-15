a03b9de1876803e7f6210aae611be2bbdbd6e931
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-15 17:41:42 +0100

    Schema fixes and changes

--- Database/banking_schema.sql
+++ Database/banking_schema.sql
@@ -4,13 +4,13 @@ CREATE TABLE customers (
     last_name varchar(100) NOT NULL,
     address varchar(200) NOT NULL,
     username varchar(50) NOT NULL,
-    password binary(32) NOT NULL
+    password varchar(64) NOT NULL
 );
 
 CREATE TABLE accounts (
     id uuid PRIMARY KEY,
     owner uuid NOT NULL REFERENCES customers(id),
-    name varcher(100) NOT NULL,
+    name varchar(100) NOT NULL,
     balance bigint NOT NULL,
     frozen bool
 );
@@ -21,19 +21,19 @@ CREATE TABLE transactions (
     amount bigint NOT NULL,
     sender uuid NOT NULL REFERENCES accounts(id),
     receiver uuid NOT NULL REFERENCES accounts(id),
-    timestamp datetime NOT NULL
+    timestamp timestamptz NOT NULL
 );
 
 CREATE TABLE withdrawals (
     id uuid PRIMARY KEY,
-    account_id uuid NOT NULL REFERENCES accounts(id),
+    account uuid NOT NULL REFERENCES accounts(id),
     amount bigint NOT NULL,
-    timestamp datetime NOT NULL
+    timestamp timestamptz NOT NULL
 );
 
 CREATE TABLE deposits (
     id uuid PRIMARY KEY,
-    account_id uuid NOT NULL REFERENCES accounts(id),
+    account uuid NOT NULL REFERENCES accounts(id),
     amount bigint NOT NULL,
-    timestamp datetime NOT NULL
+    timestamp timestamptz NOT NULL
 );
