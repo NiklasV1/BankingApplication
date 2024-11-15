5bcc184f2406623da83268059bfceedc0cc3bacb
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-15 16:43:25 +0100

    Customer Entity

--- Backend/banking-server/src/main/java/org/niklasv1/banking/model/Customer.java
+++ Backend/banking-server/src/main/java/org/niklasv1/banking/model/Customer.java
@@ -1,4 +1,29 @@
 package org.niklasv1.banking.model;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "customers")
 public class Customer {
+
+    @Id
+    @GeneratedValue
+    public UUID id;
+
+    @Column(name = "first_name", nullable = false, length = 100)
+    public String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 100)
+    public String lastName;
+
+    @Column(nullable = false, length = 200)
+    public String address;
+
+    @Column(nullable = false, length = 50)
+    public String username;
+
+    @Column(nullable = false, length = 32)
+    public byte[] password;
 }
