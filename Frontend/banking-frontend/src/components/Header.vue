3afadad7af77eb933080f80b56b787b94bed027f
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-16 15:53:10 +0100

    frontend cleanup

---
+++ Frontend/banking-frontend/src/components/Header.vue
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<template>
+    <main>
+        <div class="bg-green-800 h-full flex items-center">
+            <h1 class="text-center font-bold text-5xl text-white w-full">Banking App</h1>
+        </div>
+    </main>
+</template>
