16d4982838900e2ed40670e68d05cb6dc554cbd0
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-16 23:18:56 +0100

    visual update

--- Frontend/banking-frontend/src/components/Login.vue
+++ Frontend/banking-frontend/src/components/Login.vue
@@ -1,29 +1,29 @@
 <template>
     <main class="w-full h-full bg-slate-200 flex items-center justify-center">
-        <form class="flex flex-col gap-2 min-w-80 max-w-96 border-4 rounded-lg border-green-500 bg-white">
+        <form class="flex flex-col gap-2 min-w-80 max-w-96 border-4 rounded-lg border-slate-800 bg-slate-800 p-2">
 
             <!-- Header -->
-            <h1 class="text-gray-800 h12 text-center w-full font-semibold text-lg pt-2">Customer Login</h1>
-            <hr class="h-0.5 border-2 border-green-500">
+            <h1 class="text-white h12 text-center w-full font-semibold text-lg">Customer Login</h1>
+            <hr class="h-0 border-t-2 border-white my-1">
 
             <!-- Username -->
             <div class="flex flex-col gap-1 px-2">
-                <label class="text-gray-800 font-semibold">Username</label>
-                <input class="text-gray-800 placeholder:text-gray-400 w-full h-12 border-4 border-gray-300 rounded-lg p-2 focus:border-green-300 focus:outline-none" type="text" placeholder="Username"/>
+                <label class="text-white font-semibold">Username</label>
+                <input class="bg-slate-800 text-white placeholder:text-gray-400 w-full h-12 border-2 border-white rounded-lg p-2 focus:border-blue-800 focus:outline-none" type="text" placeholder="Username"/>
             </div>
 
             <!-- Password -->
             <div class="flex flex-col gap-1 px-2">
-                <label class="text-gray-800 font-semibold">Password</label>
-                <input class="text-gray-800 placeholder:text-gray-400 w-full h-12 border-4 border-gray-300 rounded-lg p-2 focus:border-green-300 focus:outline-none" type="password" placeholder="Password"/>
+                <label class="text-white font-semibold">Password</label>
+                <input class="bg-slate-800 text-white placeholder:text-gray-400 w-full h-12 border-2 border-white rounded-lg p-2 focus:border-blue-800 focus:outline-none" type="password" placeholder="Password"/>
             </div>
-            <hr class="h-0.5 border-2 border-green-500 my-1">
+            <hr class="h-0 border-t-2 border-white my-2">
             
 
             <!-- Buttons -->
             <div class="flex gap-4 items-stretch px-2 pb-2">
-                <button class="text-white bg-green-500 rounded-lg p-2 text-lg basis-1/2 h-12 font-semibold border-green-500 border-4 hover:bg-green-600 hover:border-green-600 focus:outline-none focus:border-green-600 focus:bg-green-600" @click="loginProcess">Login</button>
-                <button class="text-white bg-green-500 rounded-lg p-2 text-lg basis-1/2 h-12 font-semibold border-green-500 border-4 hover:bg-green-600 hover:border-green-600 focus:outline-none focus:border-green-600 focus:bg-green-600" @click="registerRedirect">Register</button>
+                <button class="text-white bg-slate-800 rounded-lg p-2 text-lg basis-1/2 h-12 font-semibold border-white border-2 hover:bg-blue-800 hover:border-blue-800 focus:outline-none focus:border-blue-800 focus:bg-blue-800" @click="loginProcess">Login</button>
+                <button class="text-white bg-slate-800 rounded-lg p-2 text-lg basis-1/2 h-12 font-semibold border-white border-2 hover:bg-blue-800 hover:border-blue-800 focus:outline-none focus:border-blue-800 focus:bg-blue-800" @click="registerRedirect">Register</button>
             </div>
         </form>
     </main>
--- Frontend/banking-frontend/src/components/menus/Register.vue
+++ Frontend/banking-frontend/src/components/menus/Register.vue
@@ -23,49 +23,49 @@ const address = ref("");
 
 <template>
     <main class="w-full h-full bg-slate-200 flex items-center justify-center">
-        <form class="flex flex-col gap-2 min-w-96 border-4 rounded-lg border-green-500 bg-white">
+        <form class="flex flex-col gap-2 min-w-96 border-2 rounded-lg border-slate-800 p-2 bg-slate-800">
             <!-- Header -->
-            <h1 class="text-gray-800 h12 text-center w-full font-semibold text-lg pt-2">Customer Registration</h1>
-            <hr class="h-0.5 border-2 border-green-500">
+            <h1 class="text-white h12 text-center w-full font-semibold text-lg">Customer Registration</h1>
+            <hr class="h-0 border-t-2 border-white">
 
-            <div class="flex">
+            <div class="flex gap-4">
                 <!-- Username -->
-                <div class="flex flex-col gap-1 px-2">
-                    <label for="username" class="text-gray-800 font-semibold">Username</label>
-                    <input v-bind="username" id="username" class="text-gray-800 placeholder:text-gray-400 w-full h-12 border-4 border-gray-300 rounded-lg p-2 focus:border-green-300 focus:outline-none" type="text" placeholder="Username"/>
+                <div class="flex flex-col gap-1">
+                    <label for="username" class="text-white font-semibold">Username</label>
+                    <input v-bind="username" id="username" class="text-white bg-slate-800 placeholder:text-gray-400 w-full h-12 border-2 border-white rounded-lg p-2 focus:border-blue-800 focus:outline-none" type="text" placeholder="Username"/>
                 </div>
     
                 <!-- Password -->
-                <div class="flex flex-col gap-1 px-2">
-                    <label for="password" class="text-gray-800 font-semibold">Password</label>
-                    <input v-bind="password" id="password" class="text-gray-800 placeholder:text-gray-400 w-full h-12 border-4 border-gray-300 rounded-lg p-2 focus:border-green-300 focus:outline-none" type="password" placeholder="Password"/>
+                <div class="flex flex-col gap-1">
+                    <label for="password" class="text-white font-semibold">Password</label>
+                    <input v-bind="password" id="password" class="text-white bg-slate-800 placeholder:text-gray-400 w-full h-12 border-2 border-white rounded-lg p-2 focus:border-blue-800 focus:outline-none" type="password" placeholder="Password"/>
                 </div>
             </div>
 
-            <div class="flex">
+            <div class="flex gap-4">
                 <!-- First Name -->
-                <div class="flex flex-col gap-1 px-2">
-                    <label for="firstName" class="text-gray-800 font-semibold">First name</label>
-                    <input v-bind="firstName" id="firstName" class="text-gray-800 placeholder:text-gray-400 w-full h-12 border-4 border-gray-300 rounded-lg p-2 focus:border-green-300 focus:outline-none" type="text" placeholder="First Name"/>
+                <div class="flex flex-col gap-1">
+                    <label for="firstName" class="text-white font-semibold">First name</label>
+                    <input v-bind="firstName" id="firstName" class="text-white bg-slate-800 placeholder:text-gray-400 w-full h-12 border-2 border-white rounded-lg p-2 focus:border-blue-800 focus:outline-none" type="text" placeholder="First Name"/>
                 </div>
     
                 <!-- Last Name -->
-                <div class="flex flex-col gap-1 px-2">
-                    <label for="lastName" class="text-gray-800 font-semibold">Last name</label>
-                    <input v-bind="lastName" id="lastName" class="text-gray-800 placeholder:text-gray-400 w-full h-12 border-4 border-gray-300 rounded-lg p-2 focus:border-green-300 focus:outline-none" type="text" placeholder="Last Name"/>
+                <div class="flex flex-col gap-1">
+                    <label for="lastName" class="text-white font-semibold">Last name</label>
+                    <input v-bind="lastName" id="lastName" class="text-white bg-slate-800 placeholder:text-gray-400 w-full h-12 border-2 border-white rounded-lg p-2 focus:border-blue-800 focus:outline-none" type="text" placeholder="Last Name"/>
                 </div>
             </div>
 
             <!-- Address -->
-            <div class="flex flex-col gap-1 px-2">
-                <label for="address" class="text-gray-800 font-semibold">Address</label>
-                <input v-bind="address" id="address" class="text-gray-800 placeholder:text-gray-400 w-full h-12 border-4 border-gray-300 rounded-lg p-2 focus:border-green-300 focus:outline-none" type="text" placeholder="Address"/>
+            <div class="flex flex-col gap-1 ">
+                <label for="address" class="text-white font-semibold">Address</label>
+                <input v-bind="address" id="address" class="text-white bg-slate-800 placeholder:text-gray-400 w-full h-12 border-2 border-white rounded-lg p-2 focus:border-blue-800 focus:outline-none" type="text" placeholder="Address"/>
             </div>                
-            <hr class="h-0.5 border-2 border-green-500 my-1">
+            <hr class="h-0 border-t-2 border-white my-1">
             
             <!-- Buttons -->
             <div class="flex gap-4 items-stretch px-2 pb-2">
-                <button class="text-white bg-green-500 rounded-lg p-2 text-lg basis-full h-12 font-semibold border-green-500 border-4 hover:bg-green-600 hover:border-green-600 focus:outline-none focus:bg-green-600 focus:border-green-600" @click="registerProcess">Register</button>
+                <button class="text-white bg-slate-800 rounded-lg p-2 text-lg w-full h-12 font-semibold border-white border-2 hover:bg-blue-800 hover:border-blue-800 focus:outline-none focus:border-blue-800 focus:bg-blue-800" @click="registerProcess">Register</button>
             </div>
         </form>
     </main>
